50fef45cb9510f815922f6be7b9994148fa09577
camachoitzel <camachoitzel101@gmail.com>
2025-05-31 15:23:47 -0700

    Practice for Python Learning

---
+++ Numbers_main.py
@@ -0,0 +1,29 @@
+#Fundemental data types
+#int
+#float
+
+#gives you the type of num type it is
+# print(type(2 + 4))
+# print (type(2 - 4))
+# print (type(2 * 4))
+# print (type(2 / 4))
+
+# print (type(9.9+1.1))
+
+# 2^3
+# print(2 ** 3)
+
+#rounds down to int
+# print (3 // 4)
+
+#remainder
+# print(5 % 4)
+
+#Math Functions
+
+# round the num
+print(round(3.1))
+print(round(3.9))
+
+# absolute value
+print(abs(-20))
---
+++ Operator_precedence.py
@@ -0,0 +1,11 @@
+# Operator Precedence
+#Ex.  20 - 3 * 4
+
+print(20 - 3 * 4)
+
+# Order of operations
+# () parenthesis
+# ** power of
+# * / mult and div
+# + - add & subtract
+print((20 - 3) + 2 ** 2)
