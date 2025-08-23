7989d6f2040a0046608083f25a4eb1af28f33fd1
camachoitzel <camachoitzel101@gmail.com>
2025-08-22 22:17:56 -0700

    Finished up set methods

--- Set_methods.py
+++ Set_methods.py
@@ -18,12 +18,23 @@ print(my_set) #prints {1,2,3} b/c my_set was modified
 
 # .intersection()
 print(my_set.intersection(your_set)) #prints {4,5} b/c those are the common nums in both sets
+#shorthand for intersection
+print(my_set & your_set)
 
 # .isdisjoint()
 print(my_set.isdisjoint(your_set)) #returns False b/c they do have elements in common, would return True if nothing in common
 
 # .issubset()
+#if my_set = {4,5}
+my_set.issubset(your_set) #returns True b/c all of my_set is in your_set
 
 # .issuperset()
+#if my_set = {4,5}
+print(my_set.issuperset(your_set)) #returns false b/c my_set does not encompass all of your_set
+print(your_set.issuperset(my_set)) #returns True b/c your_set encompasses all of my_set
 
 # .union()
+# union unites sets together w/o duplicates and returns a new set diff from the 2 it combined
+print(my_set.union(your_set)) #prints {1,2,3,4,5,6,7,8,9,10}
+#shorthand for union
+print(my_set | your_set)
