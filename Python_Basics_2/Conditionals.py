4100b5c371acfa26b6e48845308cecf9b684d246
camachoitzel <camachoitzel101@gmail.com>
2025-08-24 22:31:31 -0700

    Practicing conditional statements

---
+++ Python_Basics_2/Conditionals.py
@@ -0,0 +1,12 @@
+# conditionals
+is_old = False
+is_licenced = False
+
+if is_old:
+    print("You are old enough to drive!")
+elif is_licenced:
+    print("You can drive now!")
+else:
+    print("You are not of age!")
+
+print("okoko")
